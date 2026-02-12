8d59a199b9c3d7bbf6ee23334bd48413ec81b11e
Madson <madson.mms@gmail.com>
2026-02-11 21:30:17 -0300

    Feature: start making character look to each direction based on mouse poisition

--- player.gd
+++ player.gd
@@ -7,7 +7,8 @@ extends CharacterBody2D
 @onready var input_dir : Vector2 = Vector2.ZERO
 @export var move_speed : float = 100.00
 
-var last_facing_direction : Vector2 = Vector2(0, -1) 
+var walking_direction : Vector2 = Vector2(0, -1)
+var facing_direction : Vector2 = Vector2.DOWN
 
 func _physics_process(_delta: float) -> void:
 	
@@ -18,16 +19,32 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 	
 	var idle = !velocity
+	var camera = get_viewport().get_camera_2d()
+	var mouse_position = camera.get_local_mouse_position()
 	
 	if !idle:
-		last_facing_direction = velocity.normalized()
+		walking_direction = velocity.normalized()
 		
-	if last_facing_direction.x < 0:
-		player_sprite.scale.x = -1
-	else:
-		player_sprite.scale.x = 1
+		if walking_direction.x > 0 and mouse_position.x < 0:
+			player_sprite.scale.x = -1
+			facing_direction = Vector2(-1, 0)
+			
+		
+		print_debug(walking_direction.x, mouse_position.x)
+		
 		
 	
-	animation_tree.set("parameters/idle/blend_position", last_facing_direction)
-	animation_tree.set("parameters/walk/blend_position", last_facing_direction)
+	if idle:
+		if mouse_position.x < 0:
+			player_sprite.scale.x = -1
+		else:
+			player_sprite.scale.x = 1
+		animation_tree.set("parameters/idle/blend_position", mouse_position)
+	
+	
+	
+		
+	
+	#animation_tree.set("parameters/idle/blend_position", last_facing_direction)
+	animation_tree.set("parameters/walk/blend_position", facing_direction)
 	
